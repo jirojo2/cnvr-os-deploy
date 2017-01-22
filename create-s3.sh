58d86bc810e23f7e8b7894614da3fa933d218767
José Ignacio Rojo <jirojo2@gmail.com>
2017-01-22 17:35:17 +0100

    Fixed S3 hostname

--- create-s3.sh
+++ create-s3.sh
@@ -9,5 +9,5 @@ source /root/bin/demo-openrc.sh
 
 IP_S3=10.1.1.8
 
-openstack server create --flavor m1.smaller --image xenial-server-cloudimg-amd64-vnx s2 --nic net-id=Net1 --key-name scenario --security-group open --user-data cnvr-os-deploy/cloud-init/www/user_data
+openstack server create --flavor m1.smaller --image xenial-server-cloudimg-amd64-vnx s3 --nic net-id=Net1 --key-name scenario --security-group open --user-data cnvr-os-deploy/cloud-init/www/user_data
 neutron lbaas-member-create --subnet Subnet1 --address $IP_S3 --protocol-port 80 pool1
